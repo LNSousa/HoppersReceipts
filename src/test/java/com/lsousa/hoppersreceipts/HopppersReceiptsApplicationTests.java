2d24c5363f4438dcaeeb0c2df703dbebd6303996
Lucas Sousa <lucasns241@gmail.com>
2023-06-13 18:19:47 -0400

    first commit

---
+++ src/test/java/com/lsousa/hoppersreceipts/HopppersReceiptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsousa.hoppersreceipts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopppersReceiptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
